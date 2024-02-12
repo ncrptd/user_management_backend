0fd8d3aab85f2405a0f89eaa47f62336d90eed6a
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2024-02-12 16:36:01 +0530

    ref:

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world');
+    res.send('Hello, server');
 });
 
 app.use('/api/v1/auth', authRoutes);
